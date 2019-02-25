58f4697003332ed958b237a0812eb4b61a38472c
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-02-25 10:46:00 +0100

    chore: use containsElement in open to improve shadow-dom support

--- src/index.ts
+++ src/index.ts
@@ -1809,9 +1809,7 @@ function FlatpickrInstance(
       if (
         self.config.allowInput === false &&
         (e === undefined ||
-          !(<HTMLDivElement>self.timeContainer).contains(
-            e.relatedTarget as Node
-          ))
+          !containsElement(<HTMLDivElement>self.timeContainer, e.relatedTarget as Node))
       ) {
         setTimeout(() => (<HTMLInputElement>self.hourElement).select(), 50);
       }
